a304adf5e5204d6535abf2f3be66e36ea850d1b9
sammy <emmafs14@gmail.com>
2025-03-03 16:44:29 +0100

    Added feature to see tasks

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ class Task
 
 class TodoList{
 	
-public:
+	public:
 	std::vector<Task> todolist;
 	void AddTask(const std::string & description){
 		todolist.push_back(Task(description));
@@ -26,22 +26,92 @@ public:
 int main() {
 
 TodoList todo;
-std:: string input;
 
-std::cout<< "What task would you like to add?, write done when you are finished"<<std::endl;
-
+	//Choosing the task
 	
-	while (true) {
-		std::cout << " - ";
-		std::getline (std::cin, input);
+	bool isRunning = true;
 	
-		if (input=="done") {
-		break;
+	while (isRunning = true){
 	
+		int chosenFunction = 0;
+		
+		if (chosenFunction == 0){
+			std::cout << "Welcome!" << std::endl;
+			std::cout << "Choose a function to get started:" << std::endl;
+			std::cout << "1. Add task(s)" << std::endl;
+			std::cout << "2. See all tasks" << std::endl;
+			std::cout << "3. Edit task" << std::endl;
+			std::cout << "4. Delete task" << std::endl;
+			std::cout << "5. Mark task as done" << std::endl;
+			std::cout << "6. Exit list" << std::endl;
+			std::cout << "*Please enter the number of the function, you'd like to use:" << std::endl;
+			
+			std::cin >> chosenFunction;
 		}
-		todo.AddTask(input);
-	}
+		//Add task
+		else if (chosenFunction == 1){
+			std::cout<< "What task would you like to add?, write 'done' when you are finished"<<std::endl;
 
+			while (true) {
+				std::cout << " - ";
+				std::string input;
+				std::getline (std::cin, input);
+			
+				if (input == "done") {
+				break;
+			
+				}
+				todo.AddTask(input);
+			}
+			chosenFunction = 0;
+		}
+		
+		//See all tasks
+		else if (chosenFunction == 2){
+			if (todo.empty()){
+				std::cout << "No tasks currently available\nPlease input new tasks" << std::endl;
+				chosenFunction = 0;
+			}
+			else if (!TodoList.empty()){
+				int sizeOfList = TodoList.size();
+				for (int i = 0; i < sizeOfList; ++i){
+					std::cout << "- " << TodoList[i] << std::endl;
+				}
+				std::cout << "Write 'done' to return to functions" << std::endl;
+				std::string input = "";
+				std::cin >> input;
+				if (input == "done"){
+					chosenFunction = 0;
+				}
+			}
+		}
+			
+		//Edit task
+		else if (chosenFunction == 3){
+			//Edit task
+		}
+			
+		//Delete task
+		else if (chosenFunction == 4){
+			//Delete task
+		}
+		
+		//Mark task as done
+		else if (chosenFunction == 5){
+			//Mark as done
+		}
+		
+		//Exit the loop
+		else if (chosenFunction == 6){
+			std::cout << "Goodbye!" << std::endl;
+			isRunning = false;
+		}
+		
+		//Other input has been given to choose task
+		else {
+			std::cout << "Please type one of the valid numbers" << std::endl;
+		}
+	}
     return 0;
 }
 
